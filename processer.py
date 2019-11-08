6e15225084a3505283af1f2304639aaa2156e732
鬼王 <THRAEX@aliyun.com>
2019-11-08 17:44:14 +0800

    feat: base64 ssr

--- processer.py
+++ processer.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import json, base64
+import json, base64, time
+
+CODING = 'UTF-8'
 
 class Processer(object):
 	def __init__(self):
@@ -12,21 +14,36 @@ class Processer(object):
 		with open('configs/config.json') as f:
 			configs = json.load(f)
 
-		for cfg in configs:
-			b64_pass = self.b64encode(cfg['password'])
-			b64_obfsparam = self.b64encode(cfg['obfs_param'])
-			b64_protoparam = self.b64encode(cfg['protocol_param'])
-			b64_group = self.b64encode(cfg.get('group', ''))
-			b64_remarks = self.b64encode(cfg.get('remarks', ''))
+		target = 'configs/ssr-links'
+		with open(target, 'w') as f:
+			for cfg in configs:
+				b64_pass = self.b64encode(cfg['password'])
+				b64_obfsparam = self.b64encode(cfg['obfs_param'])
+				b64_protoparam = self.b64encode(cfg['protocol_param'])
+				b64_group = self.b64encode(cfg.get('group', ''))
+				b64_remarks = self.b64encode(cfg.get('remarks', ''))
 
-			b64_link = self.b64encode(self.link % (cfg.get('server'), cfg.get('server_port'), cfg.get('protocol'),
-				cfg.get('method'), cfg.get('obfs'), b64_pass, b64_obfsparam,
-				b64_protoparam, b64_remarks, b64_group))
+				b64_link = self.b64encode(self.link % (cfg.get('server'), cfg.get('server_port'), 
+					cfg.get('protocol'), cfg.get('method'), cfg.get('obfs'), b64_pass, b64_obfsparam, 
+					b64_protoparam, b64_remarks, b64_group))
 
-			print('ssr://%s' % b64_link)
+				final_link = ('ssr://%s' % b64_link)
+				print(final_link)
+				f.write(final_link)
+				f.write('\n')
+
+		print('******************************************')
+		with open(target) as f:
+			data = f.read().encode(CODING)
+			print('%s\n' % data)
+			final = base64.b64encode(data).decode(CODING) 
+			print(final)
+
+			with open('configs/ssr-dist', 'w') as fo:
+				fo.write(final)
 
 	def b64encode(self, s):
-		return base64.urlsafe_b64encode(s.encode('utf-8')).decode('utf-8').rstrip('=')
+		return base64.urlsafe_b64encode(s.encode(CODING)).decode(CODING).rstrip('=')
 
 if __name__ == '__main__':
 	Processer().run()
