9178c09274d13b44b9bd4e191d9770508a564ac8
鬼王 <THRAEX@aliyun.com>
2019-11-09 16:55:48 +0800

    refactor: loading configuration

--- printc.py
+++ printc.py
@@ -1,33 +1,38 @@
 # -*- coding: utf-8 -*-
 
-def underline(s):
+def underline(s = ''):
 	return '\033[4;37;40m%s\033[0m' % s
 
-def red(s):
+def red(s = ''):
 	return '\033[0;31;40m%s\033[0m' % s
 
-def green(s):
+def green(s = ''):
 	return '\033[0;32;40m%s\033[0m' % s
 
-def yellow(s):
+def yellow(s = ''):
 	return '\033[0;33;40m%s\033[0m' % s
 
-def blue(s):
+def blue(s = ''):
 	return '\033[0;34;40m%s\033[0m' % s
 
-def cyan(s):
+def cyan(s = ''):
 	return '\033[0;36;40m%s\033[0m' % s
 
-def error(s):
+def error(s = ''):
 	print('[%s] %s' % (red('ERROR'), s))
 
-def warning(s):
+def warning(s = ''):
 	print('[%s] %s' % (yellow('WARNING'), s))
 
-def info(s):
+def info(s = ''):
 	print('[%s] %s' % (blue('INFO'), s))
 
+def infoln(s = ''):
+	print('[%s] %s' % (blue('INFO'), s))
+	info()
+
 if __name__ == '__main__':
+	print(red())
 	print(red('red'))
 	print(green('green'))
 	print(yellow('yellow'))
--- processer.py
+++ processer.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import json, base64, time
+import json, base64, printc
 from server import Start
 
 CODING = 'UTF-8'
@@ -8,9 +8,10 @@ CODING = 'UTF-8'
 class Processer(object):
 	def __init__(self):
 		super(Processer, self).__init__()
+		# Please refer to: https://github.com/shadowsocksrr/shadowsocks-rss/wiki/SSR-QRcode-scheme
 		self.link = '%s:%d:%s:%s:%s:%s/?obfsparam=%s&protoparam=%s&remarks=%s&group=%s'
 
-	def run(self):
+	def run_old(self):
 		with open('configs/config.json') as f:
 			configs = json.load(f)
 
@@ -43,6 +44,41 @@ class Processer(object):
 				fo.write(final)
 
 		Start().UP()
+
+	def run(self):
+		printc.infoln('Loading configuration file...')
+
+		printc.info('-------------------< %s >------------------' % 
+			printc.cyan('ssr-subscribe-generator'))
+		printc.info('Subscription Generator of SSR')
+		printc.infoln('---------------------------[ %s ]---------------------------' %
+			printc.cyan('Base64'))
+
+		self.configs()
+
+	def configs(self):
+		cfg_path = 'configs/config.json'
+
+		printc.info('---------------------< %s >--------------------' % 
+			printc.cyan(cfg_path))
+
+		configs = None
+		try:
+			with open(cfg_path) as f: configs = json.load(f)
+
+			total = len(configs)
+
+			if total > 0:
+				printc.infoln('Total: %s.' % total)
+
+				for cfg in configs:
+					printc.infoln(cfg)
+			else:
+				printc.warning('No server node information.')
+		except Exception as e:
+			printc.error(e)
+
+		return configs
 
 	def b64encode(self, s):
 		return base64.urlsafe_b64encode(s.encode(CODING)).decode(CODING).rstrip('=')
